f9cf5fb99e6f9c70abe5d3cfd349d224a531be1c
LEVI CARNEIRO ALVES <202002438045@alunos.estacio.br>
2022-09-15 20:48:43 -0300

    Alteração 15/09

--- App.js
+++ App.js
@@ -1,18 +1,19 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View, Image} from 'react-native';
-import sunflower from './assets/levi.png';
+
+import imagem from './assets/motoboy.png';
 
 export default function App() {
   return (
    <> 
     <View style={styles.container}>
-      <Image source={sunflower} style={styles.foto}></Image>
-      <Text style={styles.FirstText}><a href="https://youtu.be/mK2xhbj0MFw" onclick="Function();">Clique aqui</a></Text>
+      <Text style={styles.FirstText}> HELP FOOD </Text>
+      <Image source={imagem} style={styles.foto}></Image>
       <StatusBar style="auto"/>
     </View>
     
     <View style={styles.container2}>
-      <Text style={styles.Text}>^ Clique no link acima ^</Text>
+      <Text style={styles.Text}></Text>
       <StatusBar style="auto"/>
     </View>
   </>
@@ -21,14 +22,14 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#DAA520',
+    backgroundColor: '#2A431B',
     alignItems: 'center',
     justifyContent: 'center',
   },
 
     container2: {
       flex: 1,
-      backgroundColor: '#DAA520',
+      backgroundColor: '#2A431B',
       alignItems: 'center',
       justifyContent: 'center',
     },
---
+++ src/components/AppName/index.js
@@ -0,0 +1,1 @@
+import { text, StyleSheet } from 'react-native';
